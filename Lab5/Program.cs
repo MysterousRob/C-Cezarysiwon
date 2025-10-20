621835088d27fd6b4ebdaed7121c03ff49ccc656
MysterousRob <robertjohngasior@gmail.com>
2025-10-21 00:31:05 +0200

    rename in classnnotes for leksje to lekcje

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO.Compression;
+namespace Lab5;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // ---- Uncomment as u go along ---- //
+
+        //Zadanie1();
+        //Zadanie2();
+        //Zadanie3();
+        //Zadanie4();
+        //Zadanie5();
+    }
+
+    public static void Zadanie1()
+    {
+        
+    }
+}
