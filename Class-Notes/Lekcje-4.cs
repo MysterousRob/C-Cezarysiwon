32333e81a41e431f18acba6e3e2826400b70a64e
MysterousRob <robbertjohngasior@gmail.com>
2025-10-29 10:17:57 +0100

    maikin init commit dfor lek 4

--- Class-Notes/Lekcje-4.cs
+++ Class-Notes/Lekcje-4.cs
@@ -4,9 +4,22 @@ namespace Lekcje4;
 
 class Program
 {
-    static decimal[] lastWeekExpense = new decimal[10];
+    static decimal[] lastWeekExpense = {15.5m, 25.3m, 40m, 51.45, 100, 21, 0};
     public static void Main(string[] args)
     {
-        System.Console.WriteLine("hwlloq world")
+        System.Console.WriteLine("hwlloq world");
+    }
+
+    public static void Example1()
+    {
+        System.Console.WriteLine($"wydadtkiz wtorku {lastWeekExpense [1]}");
+        System.Console.WriteLine($"wydatki z niedzieli{lastWeekExpense[^1]}");
+        System.Console.WriteLine($"trzecie od konca {lastWeekExpense[^3]}");
+
+        System.Console.WriteLine($"ile dni w tablicy {lastWeekExpense.Length}");
+
+
+        lastWeekExpense[0] += 5;
+        System.Console.WriteLine($"wyydadtki z poniedzialku {lastWeekExpense[0]}");
     }
 }
