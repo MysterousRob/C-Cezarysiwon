6d8e1f1027d48d973ca08db76da6232b8fd85042
MysterousRob <robbertjohngasior@gmail.com>
2025-10-29 10:10:27 +0100

    iinit lekcje 4

--- Class-Notes/Lekcje-4.cs
+++ Class-Notes/Lekcje-4.cs
@@ -4,6 +4,7 @@ namespace Lekcje4;
 
 class Program
 {
+    static decimal[] lastWeekExpense = new decimal[10];
     public static void Main(string[] args)
     {
         System.Console.WriteLine("hwlloq world")
