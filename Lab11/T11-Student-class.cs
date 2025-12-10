54edf733f03a89a4934fe3af2dc94a658912fd51
MysterousRob <robertjohngasior@gmail.com>
2025-12-10 06:30:52 +0100

    started the student class

---
+++ Lab11/T11-Student-class.cs
@@ -0,0 +1,59 @@
+public class Student
+{
+    public string Name { get; set; }
+    public int Points { get; set; }
+
+    public Student(string name, int points)
+    {
+        Name = name;
+        Points = points;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} ({Points} pkt);
+    }
+}
+
+public static class BubbleSort
+{
+    public static List<Student> Sort(List<Student> students)
+    {
+        for (int i = 0; i < students.Count - 1; i++)
+        {
+            for (int j = 0; j < students.Count - i - 1; j++)
+            {
+                if (students[j].Points > students[j + 1].Points)
+                {
+                    var temp = students[j];
+                    students[j] = students[j + i];
+                    students[j + 1] = temp;
+                }
+            }
+        }
+        return students;
+    }
+}
+
+
+public static class SearchPoint
+{
+    public static List<int> BinarySearch(List<Student> sorted, int points)
+    {
+        int left = 0;
+        int right = sorted.Count - 1;
+        List<int> found = new List<int>();
+
+
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+
+            if (sorted[mid].Points == points)
+            {
+                int i = mid;
+                while (i >= 0 && sorted[i].Points == points)
+            }
+        }
+    }
+}
