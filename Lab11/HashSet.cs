83b9dcffae06d97406c452a8c97a60aadcac6dc1
MysterousRob <robertjohngasior@gmail.com>
2025-12-10 07:20:21 +0100

    finished did snth exrea

---
+++ Lab11/HashSet.cs
@@ -0,0 +1,24 @@
+public static class UniqueStudents
+{
+    public static HashSet<string> UniqueNames(List<Student> students)
+    {
+        HashSet<string> set = new HashSet<string>();
+
+        foreach (var s in students)
+            set.Add(s.Name);
+        
+        return set;
+    }
+}
+
+public static class SetOperations
+{
+    public static HashSet<string> Union(Hashset<string> a , HashSet<string> b)
+        =>new HashSet<string>(a.Union(b));
+
+    public static HashSet<string> Intersect(HashSet<string> a, HashSet<string> b) 
+        => new HashSet<string>(a.Intersect(b));
+    
+    public static HashSet<string> Difference(HashSet<string> a, HashSet<string> b)
+        => new HashSet<string>(a.Except(b)); 
+}
