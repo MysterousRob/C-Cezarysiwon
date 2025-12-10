2ccf220cc5a8d9e71c21f6e2c27722bcdb618dcc
MysterousRob <robertjohngasior@gmail.com>
2025-12-10 06:44:29 +0100

    finished dictionary

---
+++ Lab11/t11-2-DictionaryandGrouping.cs
@@ -0,0 +1,18 @@
+public static class StudentDatabase
+{
+    public static Dictionary<char, List<Student>> GroupByFirstLetter(List<Student> students)
+    {
+        Dictionary<char, List<Student>> result = new Dictionary<char, List<Student>>();
+
+        foreach (var s in students)
+        {
+            char first = char.ToUpper(s.Name[0]);
+
+            if (!result.ContainsKey(first))
+                result[first] = new List<Student>(); 
+
+            result[first].Add(s);
+        }
+        return result;
+    }
+}
